70b029cbc53db6ea13a6f6a028ca925c7ea5bb6e
Kaidenleaf <129576524+Kaidenleaf@users.noreply.github.com>
2025-12-22 22:35:25 -0600

    fix: outputs

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   outputs = { self, nixpkgs, ... }: {
     nixosConfigurations.zenith = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
+      #specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
       ];
