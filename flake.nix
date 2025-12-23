579ab78f2d4c38e1520689c38648b941c0b6d648
Kaidenleaf <129576524+Kaidenleaf@users.noreply.github.com>
2025-12-22 22:32:57 -0600

    initial commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "NixOS config with Flakes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.zenith = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
