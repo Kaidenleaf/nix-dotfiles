9190b3da1f953cb8a1c9dffa09e9af8176e31db9
Kaidenleaf <129576524+Kaidenleaf@users.noreply.github.com>
2025-12-23 13:42:28 -0600

    add: data disk

---
+++ filesystem.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+
+{
+  fileSystems."/home/kaiden/datos" = {
+    device = "/dev/disk/by-uuid/fab5fde2-a0ab-4121-9d55-be3daf827020";
+    fsType = "ext4"; # O ntfs-3g, exfat, etc.
+    options = [ "defaults" "user" "rw" "nofail" ];
+  };
+}
